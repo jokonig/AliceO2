4bd57654d50ca148cba950dd8634b6b58985e206
jokonig <jokonig@cern.ch>
2024-08-09 17:47:54 +0200

    [EMCAL-1135] Throw exception in case starttime is to large or small
    - This commit is an extention for PR: https://github.com/AliceO2Group/AliceO2/pull/13326
    - Move error class from base to reconstruction
    - added handling of error RawToCell converter: handleFastORStartTimeErrors
    - add error type to TRUDecodingError to indicate invalid starttime

--- Detectors/EMCAL/reconstruction/src/FastORTimeSeries.cxx
+++ Detectors/EMCAL/reconstruction/src/FastORTimeSeries.cxx
@@ -12,7 +12,7 @@
 #include <algorithm>
 #include <iostream>
 #include "EMCALReconstruction/FastORTimeSeries.h"
-#include "EMCALBase/TRUDecodingErrors.h"
+#include "EMCALReconstruction/TRUDecodingErrors.h"
 
 using namespace o2::emcal;
 
@@ -20,6 +20,9 @@ void FastORTimeSeries::fillReversed(const gsl::span<const uint16_t> samples, uin
 {
   if (starttime >= 14) {
     throw FastOrStartTimeInvalidException(starttime);
+  }
+  if (starttime < samples.size() - 1) {
+    throw FastOrStartTimeInvalidException(static_cast<int>(starttime) - static_cast<int>(samples.size()) + 1);
   }
   for (std::size_t isample = 0; isample < samples.size(); isample++) {
     mTimeSamples[starttime - isample] = samples[isample];
